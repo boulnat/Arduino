43af0568792f157d9de7b226f859f1411dea119c
boulnat <boulet.nathan@gmail.com>
2020-12-28 14:07:11 +0100

    adding comments

--- sequencer/sequencer.c
+++ sequencer/sequencer.c
@@ -5,15 +5,18 @@ float const MILLI = 1000.0;
 float beat_tempo = 0;
 float delta_tempo = 0;
 
+//initalizing the beat and the delta of ON/OFF
 void initialize(float beat, float delta){
-	beat_tempo = beat;
-	delta_tempo = delta;
+  beat_tempo = beat;
+  delta_tempo = delta;
 }
 
+//time frame gives the value in millisecond of OFF time depending on the beat and tempo
 float time_frame(float seconde){
-	return (((seconde/beat_tempo)*delta_tempo)*MILLI);
+  return (((seconde/beat_tempo)*delta_tempo)*MILLI);
 }
 
+//time frame gives the value in millisecond of ON time depending on the beat and tempo
 float tempo(float seconde){
-	return (((seconde/beat_tempo)-((seconde/beat_tempo)*delta_tempo))*MILLI);
+  return (((seconde/beat_tempo)-((seconde/beat_tempo)*delta_tempo))*MILLI);
 }
--- sequencer/sequencer.ino
+++ sequencer/sequencer.ino
@@ -2,9 +2,10 @@ extern "C"{
 #include "sequencer.h"
 };
 
+//pin used can be change to anay digital pin
 int pin = 5;
 
-
+//second is the time frame for the temop
 float seconde = 1.0;
 float time_f,tempo_f = 0.0;
 
@@ -14,10 +15,15 @@ unsigned long previousTime=0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(pin, OUTPUT);
+  //2.0 is the time division it means a tempo of 2 beats per second 1/2 = 0.5s
+  //0.8 (80%) is the percentage of OFF time, 0.5*0.8 = 0.4s -> LOW 0.4s - HIGH 0.1s
+  //initialize the counters to calculate the tempo
   initialize(2.0, 0.8);
-  Serial.begin(9600);
+  //getting values that have been calculated
   time_f = time_frame(seconde);
   tempo_f = tempo(seconde);
+  
+  Serial.begin(9600);
 }
 
 void loop() {
